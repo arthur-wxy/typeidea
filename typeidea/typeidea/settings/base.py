b73589d5cfbefb6c0ee82ea5f59a0a0c3d24581b
ayf_01 <arthurw@wangxiaoyuandeMacBook-Pro.local>
2020-06-18 20:54:33 +0800

    重新创建了app

--- typeidea/typeidea/settings/base.py
+++ typeidea/typeidea/settings/base.py
@@ -31,16 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'blog',
-    'config',
-    'comment',
-
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
+    'comment',
+    'config',
 ]
 
 MIDDLEWARE = [
