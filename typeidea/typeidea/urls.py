94f91d0dda9a596d928c316a35c682bcbade3520
arthur_wxy <kzzn@qq.com>
2020-06-30 21:31:30 +0800

    add urls

--- typeidea/typeidea/urls.py
+++ typeidea/typeidea/urls.py
@@ -17,9 +17,16 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
 from .custom_site import custom_site
+from blog.views import post_list, post_detail
+from config.views import links
 
 
 urlpatterns = [
+    url(r'$', post_list),
+    url(r'^category/(?P<category_id>\d+)/$', post_list),
+    url(r'^tag/(?P<tag_id>\d+)/$', post_list),
+    url(r'^post/(?P<post_id>\d+).html$', post_detail),
+    url(r'links/$', links),
     url(r'^', admin.site.urls),
     url(r'^', custom_site.urls),
     # path('admin/', admin.site.urls),
