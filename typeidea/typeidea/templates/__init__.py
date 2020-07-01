a8d5f70d9f89f1ec5696761d8e3233eee1142be1
arthur_wxy <kzzn@qq.com>
2020-07-01 21:05:16 +0800

    增加了templates

---
+++ typeidea/typeidea/templates/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""=================================================
+@Project -> File   ：typeidea -> __init__.py
+@IDE    ：PyCharm
+@Author ：ArthurWxy
+@Date   ：2020/7/1 8:58 PM
+@Desc   ：
+=================================================="""
