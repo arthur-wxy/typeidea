13b8d8b59a8a698c25d931a223557040de8d4002
arthur_wxy <kzzn@qq.com>
2020-07-08 21:36:58 +0800

    delete url configuration

--- typeidea/typeidea/urls.py
+++ typeidea/typeidea/urls.py
@@ -28,7 +28,7 @@ urlpatterns = [
     url(r'^tag/(?P<tag_id>\d+)/$', post_list, name='tag-list'),
     url(r'^post/(?P<post_id>\d+).html$', post_detail, name='post-detail'),
     url(r'links/$', links, name='links'),
-    url(r'^super_admin/', admin.site.urls, name='super-admin'),
-    url(r'^admin/', custom_site.urls, name='admin'),
+    url(r'^', admin.site.urls, name='super-admin'),
+    url(r'^', custom_site.urls, name='admin'),
     # path('admin/', admin.site.urls),
 ]
